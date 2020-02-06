cb118ba086d7e7142fc5a63bd1eef27098d02f23
Jenny Klich <jennyklich@gmail.com>
2020-02-06 12:08:06 -0700

    Add stories 4 and 7

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
 
   get '/shelters/:shelter_id/reviews/new', to: 'reviews#new';
   post '/shelters/:shelter_id', to: 'reviews#create'
+  delete '/shelters/:shelter_id/reviews/:id', to: 'reviews#destroy'
 end
