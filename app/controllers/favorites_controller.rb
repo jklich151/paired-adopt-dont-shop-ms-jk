7da59fdf8cf19d0a7a404705be217212719c30c9
Meghan Stovall <meghanstovall15@augustana.edu>
2020-02-08 20:20:26 -0700

    Finish user story 15. Add destroy all to favorites controller. Change and fix method update.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,15 @@ class FavoritesController < ApplicationController
 
   def update
     pet = Pet.find(params[:pet_id])
-    pet_id = pet.id.to_s
     favorite.add_pet(pet.id)
     session[:favorites] = favorite.contents
-      flash[:notice] = "You now have added #{pet.name} to your favorites."
-      redirect_to "/pets/#{pet.id}"
+    flash[:notice] = "You now have added #{pet.name} to your favorites."
+    
+    redirect_to "/pets/#{pet.id}"
   end
 
   def index
-    if session[:favorites] == nil
+    if session[:favorites].empty?
       @pets = nil
     else
       @pets = Pet.find(session[:favorites])
@@ -19,9 +19,16 @@ class FavoritesController < ApplicationController
 
   def destroy
     pet = Pet.find(params[:pet_id])
-    pet_id = pet.id
-    session[:favorites].delete(pet_id)
+    session[:favorites].delete(pet.id)
     flash[:notice] = "#{pet.name} has been removed from favorites."
+
     redirect_back(fallback_location: "/favorites")
+  end
+
+  def destroy_all
+    session[:favorites] = {}
+    flash[:notice] = "You no longer have any favorites."
+
+    redirect_to '/favorites'
   end
 end
